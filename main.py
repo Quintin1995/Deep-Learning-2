1bf4169e8d547391625076835f7e173955a06d25
Quintin1995 <Quintin1995>
2019-05-28 12:34:48 +0200

    added more parameters to parameters.py. Additionlly, created a densenet that can be trained on. The agent now only takes random actions form the action space.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import gym
 import universe
+from network import DENSENET
+from parameters import *
+
+
+#before we start the game we create an empty new model of the class DENSENET, described in network.py
+model = DENSENET()
+model = model.build_model()
 
 
 env = gym.make("LunarLander-v2")
-
-for i_episode in range(1):
+for i_episode in range(M_NUM_GAMES):
     observation = env.reset()
-    for t in range(1000):
+    for t in range(M_MAX_FRAMES_PER_GAME):
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        print(len(observation))
         print(observation)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
 env.close()
+
+
--- network.py
+++ network.py
@@ -4,28 +4,26 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Activation, Dropout
 
 
-class CNN: 
+class DENSENET: 
 
     def __init__(self):
         # Define parameters
-        self.h_input_shape      = (8,)                          #number of observations fed into the neural network
-        self.h_intermediate_act = 'tanh'                        #activations function used in hidden layers
-        self.h_output_act       = 'softmax'                     #activation used in the output layer
-        self.h_optimizer        = 'adam'                        #optimizer used in compilation
-        self.h_loss             = 'categorical_crossentropy'    #loss functions used in compilation
-        self.h_metrics          = ['accuracy']                  #metric to track performance
-        self.h_hidden_unit_count= 32                            #amount of hidden neurons in each hidden layer.
-        self.h_dropout_rate     = 0.5                           #amount of dropout after each hidden layer
-        self.h_output_neurons   = 3                             #number of output neurons, which should correspond to the number of actions possible in each state of the game
+        self.h_input_shape      = N_INPUT_SHAPE                 #number of observations fed into the neural network
+        self.h_intermediate_act = N_INTERMEDIATE_ACT            #activations function used in hidden layers
+        self.h_output_act       = N_OUTPUT_ACT                  #activation used in the output layer
+        self.h_optimizer        = N_OPTIMIZER                   #optimizer used in compilation
+        self.h_loss             = N_LOSS                        #loss functions used in compilation
+        self.h_metrics          = N_METRICS                     #metric to track performance
+        self.h_hidden_unit_count= N_HIDDEN_UNIT_COUNT           #amount of hidden neurons in each hidden layer.
+        self.h_dropout_rate     = N_DROPOUT_RATE                #amount of dropout after each hidden layer
+        self.h_output_neurons   = N_OUTPUT_NEURONS              #number of output neurons, which should correspond to the number of actions possible in each state of the game
         self.model              = Sequential()
-    
+        print("densenet created... without building the model yet")
+
 
     #build the whole model and return it
     def build_model(self):
         model = Sequential()
-        print("THIS IS THE MODEL SUMMARY:")
-        print(model.summary())
-        print("END MODEL SUMMARY.")
 
         #first layer
         model.add(Dense(self.h_hidden_unit_count, activation=self.h_intermediate_act, input_shape=self.h_input_shape))
@@ -40,4 +38,9 @@ class CNN:
 
         #compiling the whole model
         model.compile(loss=self.h_loss, optimizer=self.h_optimizer, metrics=self.h_metrics)
+
+        print("THIS IS THE MODEL SUMMARY:")
+        print(model.summary())
+        print("END MODEL SUMMARY.")
+
         return self.model
