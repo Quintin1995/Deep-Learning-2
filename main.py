a6501869a60a80c545b133cc72b6c6fe5fba6117
Quintin1995 <Quintin1995>
2019-05-29 15:54:22 +0200

    created the main game loop in main.py. All parameters that run the game can be found in parameters.py. A q-learning agent has been created in the class Agent(). It can replay state-nextState pairs from a memory list. A small dense neural network has been created to evaluate the valuations of a state. The valuation of a state is updated accordingt to the valuation of the next-state.

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 import gym
 from network import DENSENET
+from agent import Agent
 from parameters import *
+import numpy as np
 
 
-#before we start the game we create an empty new model of the class DENSENET, described in network.py
-model = DENSENET()
-model = model.build_model()
+#run a q-learning experiment
+def run_experiment(amount_actions, num_observations_state):
+    #loop over each game and reset the game environment
+    for game_iterator in range(M_NUM_GAMES):
+        state = env.reset()
+        state = np.reshape(state, [1, num_observations_state])
+        for frame_iterator in range(M_MAX_FRAMES_PER_GAME):
+            #renders the game if set to true
+            if M_DO_RENDER_GAME:
+                env.render()
+            action = q_learning_agent.perform_action(state)
+            next_state, reward, is_game_done, _ = env.step(action)
+
+            #penalize for ending the game
+            if not is_game_done:
+                reward = reward
+            else:
+                reward = -10
+
+            next_state = np.reshape(next_state, [1, num_observations_state])
+            state      = np.reshape(state, [1, num_observations_state])
+
+            q_learning_agent.store_in_memory(state, action, reward, next_state, is_game_done)
+            state = next_state
+            if is_game_done:
+                print("Game Number: {} of {}, Total Reward: {}, e: {:.2}".format(game_iterator, M_NUM_GAMES, frame_iterator, q_learning_agent.epsilon_max))
+                break
+            if len(q_learning_agent.state_list) > M_PLAY_BATCH_SIZE:
+                q_learning_agent.replay_from_memory(M_PLAY_BATCH_SIZE, num_observations_state)
+    env.close()
 
 
-#OBSERVATIONS in the game are structured like this:
-## [position-x, position-y, velocity-x, velocity-y, lander-angle lander-angular-velocity, leg0-ground-contact(boolean), leg1-ground-contact(boolean)]
+##########################
 
+
+#create the environment of the game
 env = gym.make("LunarLander-v2")
 
-for i_episode in range(M_NUM_GAMES):
-    observation = env.reset()
-    for t in range(M_MAX_FRAMES_PER_GAME):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        print(observation)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+#OBSERVATIONS in the game are structured like this:
+## [position-x, position-y, velocity-x, velocity-y, lander-angle, lander-angular-velocity, leg0-ground-contact(boolean), leg1-ground-contact(boolean)]
 
+print("Welcome to the game Lunar Lander Version-2!!!")
 
+amount_actions = env.action_space.n
+num_observations_state  = env.observation_space.shape[0]
+
+#create a q-learning agent
+q_learning_agent = Agent(amount_actions, num_observations_state)
+
+run_experiment(amount_actions, num_observations_state)
--- network.py
+++ network.py
@@ -8,7 +8,7 @@ class DENSENET:
 
     def __init__(self):
         # Define parameters
-        self.h_input_shape      = N_INPUT_SHAPE                 #number of observations fed into the neural network
+        self.h_input_shape      = N_INPUT_DIM                   #number of observations fed into the neural network
         self.h_intermediate_act = N_INTERMEDIATE_ACT            #activations function used in hidden layers
         self.h_output_act       = N_OUTPUT_ACT                  #activation used in the output layer
         self.h_optimizer        = N_OPTIMIZER                   #optimizer used in compilation
@@ -18,29 +18,26 @@ class DENSENET:
         self.h_dropout_rate     = N_DROPOUT_RATE                #amount of dropout after each hidden layer
         self.h_output_neurons   = N_OUTPUT_NEURONS              #number of output neurons, which should correspond to the number of actions possible in each state of the game
         self.model              = Sequential()
-        print("densenet created... without building the model yet")
-
 
     #build the whole model and return it
     def build_model(self):
-        model = Sequential()
-
+        self.model = Sequential()
         #first layer
-        model.add(Dense(self.h_hidden_unit_count, activation=self.h_intermediate_act, input_shape=self.h_input_shape))
-        model.add(Dropout(self.h_dropout_rate))
+        self.model.add(Dense(self.h_hidden_unit_count, activation=self.h_intermediate_act, input_dim=self.h_input_shape))
+        self.model.add(Dropout(self.h_dropout_rate))
 
         #second layer
-        model.add(Dense(self.h_hidden_unit_count, activation=self.h_intermediate_act))
-        model.add(Dropout(self.h_dropout_rate))
+        self.model.add(Dense(self.h_hidden_unit_count, activation=self.h_intermediate_act))
+        self.model.add(Dropout(self.h_dropout_rate))
 
         #output layer
-        model.add(Dense(self.h_output_neurons, activation=self.h_output_act))
+        self.model.add(Dense(self.h_output_neurons, activation=self.h_output_act))
 
         #compiling the whole model
-        model.compile(loss=self.h_loss, optimizer=self.h_optimizer, metrics=self.h_metrics)
+        self.model.compile(loss=self.h_loss, optimizer=self.h_optimizer, metrics=self.h_metrics)
 
         print("THIS IS THE MODEL SUMMARY:")
-        print(model.summary())
+        print(self.model.summary())
         print("END MODEL SUMMARY.")
 
         return self.model
